c546af1b7a43137187cc5e770d01ab6286a29dad
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-20 20:45:28 +0530

    Basics

---
+++ BasicsOfJs/01_variables.js
@@ -0,0 +1,27 @@
+const mySalary = 15000
+let myEmail = "samarth@google.com"
+var myPassword = "1234"
+myCity = "Mumbai"
+let accountState;
+
+// mySalary = 1000 TypeError: Assignment to constant variable.
+// console.log(mySalary);
+
+myEmail = "sam@123.com" //Allowed to change when LET is used
+// console.log(myEmail);
+
+myPassword = "789" //ALlowed to change
+// console.log(myPassword);
+
+myCity = "Pune"
+// console.log(myCity)
+
+console.table([mySalary, myEmail, myPassword, myCity, accountState])
+
+/*
+Prefer not to use 'var' beacuse of block scope and functional scope issue
+*/
+
+
+
+
---
+++ BasicsOfJs/02_datatypes.js
@@ -0,0 +1,26 @@
+// "use strict" treats all Js code as newer version
+
+// alert(3 + 3) we are using node js, not browser
+
+// console.log(3+
+//     3
+// );     
+// console.log("Samarth");  This should be avoided, code readablity must be high
+
+let name = "Samarth" //String datatype
+let age = 21 // number datatype
+let isLoggedIn = true
+let state; //undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, representation of empty value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
