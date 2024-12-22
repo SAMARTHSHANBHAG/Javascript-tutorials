8aae48acb91cf241ab5e2c84e71b242506397ba3
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-22 22:24:18 +0530

    --

---
+++ BasicsOfJs/03_convertionOperation.js
@@ -0,0 +1,51 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// let a = "22abc"
+// let numOfA = Number(a)
+// console.log(numOfA); //Output: NaN
+
+// let b = null
+// let numOfB = Number(b)
+// console.log(numOfB); //Output: 0
+
+// let bool = true
+// let numOfBool = Number(bool)
+// console.log(numOfBool); //Output: 1 for true, 0 for false
+
+// let name = "Samarth"
+// let numOfName = Number(name)
+// console.log(numOfName); // NaN
+
+// Note : "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = false
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // output 0
+
+// let emptyString = ""
+// let boolOfEmptyString = Boolean(emptyString)
+// console.log(boolOfEmptyString); //Output: False
+
+// let isLoggedOff = "Java"
+// let booleanIsLoggedOff = Boolean(isLoggedOff)
+// console.log(booleanIsLoggedOff);
+
+// Note: 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
