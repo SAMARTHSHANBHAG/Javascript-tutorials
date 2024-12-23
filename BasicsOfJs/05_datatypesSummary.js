35d1776a0c5275a5514a0782b8cd458402e410fe
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-23 13:24:49 +0530

    -

---
+++ BasicsOfJs/05_datatypesSummary.js
@@ -0,0 +1,35 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail = undefined // let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId); //False
+
+const bigNumber = 2379827381n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Superman", "Spiderman", "Batman"]
+
+let myObj = {
+    name: "Samarth",
+    age: 21
+}
+
+const myFunc = function(){
+    console.log("Hello world!");
+    
+}
+
+console.log(typeof bigNumber);
