5a8b65c30b377c18e58849d490951c6c191b4e25
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-23 23:38:22 +0530

    -

---
+++ BasicsOfJs/07_nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed(2)); // 100.00
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4)); //123.9
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); //Output: 4, negative ko positive karta hai, +ve same rahega
+console.log(Math.round(4.6)); // Normal round off
+console.log(Math.ceil(4.2)); // Upper(Higher) number on number line is returned
+console.log(Math.floor(-2.1)); // Lower(Lesser) number on number line is returned
+console.log(Math.min(4, 5, 1, 9)); // Output : 1
+console.log(Math.max(6, 9, 1, 10));// Output: 10
+
+// console.log(Math.random()); Returns a random value between 0 and 1
+// console.log(Math.floor(Math.random() * 10) + 1); // To always get a proper random number
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor( Math.random() * (max - min + 1)) + min);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
