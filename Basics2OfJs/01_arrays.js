4c6a8549fb98b036db1912b890e98c6b145f8e6a
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-24 12:46:53 +0530

    Array1

---
+++ Basics2OfJs/01_arrays.js
@@ -0,0 +1,50 @@
+// array
+
+// const myArray = [0, 1, 2, 3, 4, true, "Samarth"]
+// // console.log(myArray[0]);
+// const myHeroes = ["Batman", "Shaktiman"]
+// const myArray2 = new Array(6, 7, 8, 9)
+// console.log(myArray2);
+
+// Array methods
+
+const MyArray = [0, 1, 2, 3, 4, 5, 6, 7]
+
+// MyArray.push(8)
+// MyArray.push(9)
+// MyArray.pop()
+
+// // MyArray.unshift(9)
+// MyArray.shift()
+
+// console.log(MyArray.includes(5));
+// console.log(MyArray.indexOf(8));
+
+// const newArray = MyArray.join()
+
+// console.log(MyArray); // output : [0, 1, 2, 3,4, 5, 6, 7]
+// console.log(newArray); // output : 0,1,2,3,4,5,6,7  
+// console.log(typeof newArray); // String type
+
+// SLICE, SPLICE
+Myprac = [0, 1, 2, 3, 4, 5]
+
+console.log("A", Myprac); // A [ 0, 1, 2, 3, 4, 5 ]
+const myA1 = Myprac.slice(1, 3)
+
+console.log(myA1); // [1,2]
+console.log("B", Myprac); // B [ 0, 1, 2, 3, 4, 5 ]
+
+const myA2 = Myprac.splice(1, 3)
+console.log(myA2); // [1, 2, 3 ]
+console.log("C", Myprac); // C [ 0, 4, 5 ]
+
+
+
+
+
+
+
+
+
+
