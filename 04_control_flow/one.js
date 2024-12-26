8bbbdf8b80859bfbdaad8b54f2e37a827ef99496
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-26 14:05:03 +0530

    -

---
+++ 04_control_flow/one.js
@@ -0,0 +1,37 @@
+// If
+
+const IsLoggedIn = true
+const temperature = 27
+
+// if(temperature == 28) {
+//     console.log("The temperature is exact1");
+    
+// } else {
+//     console.log("The temperature is not exact");
+    
+// }
+// console.log("Execute"); // Will be executed in all cases as it is out of the condition
+// Types of comparisons : <, >, <=, >=, ==, ===, !=, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`); // ReferenceError: power is not defined. Not in scope
+// If var had been used to define power then it could have been accessed here as 'var' has global scope
+
+// Implicit if
+const balance = 1000
+// if (balance > 500) console.log("There is enough balance"), console.log("End"); // Bad code readablity
+
+
+
+
+
+
+
+
+
