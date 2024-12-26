7e038d4aa2c9f55e8d52d339f1243f27694acd1f
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-26 20:13:20 +0530

    -

---
+++ 05_iterationsOfJs/two.js
@@ -0,0 +1,26 @@
+// while loop
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(`The value is ${i}`);
+//     i = i + 2
+    
+// }
+
+// let myArr = ['hit', 'fit', 'sit']
+// let arr = 0
+
+// while (arr < myArr.length) {
+//     console.log(`The value is ${myArr[arr]}`);
+//     arr = arr + 1
+// }
+
+// Do while
+
+let score2 = 11
+
+do {
+    console.log(`Score is ${score2}`);
+    score2 ++
+    
+} while (score2 <= 10)
