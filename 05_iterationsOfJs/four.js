49c4d30264ab1337c68ac9344f878aebd4f8b514
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-26 23:28:01 +0530

    -

---
+++ 05_iterationsOfJs/four.js
@@ -0,0 +1,37 @@
+const obj8 = {
+    js : "Javascript",
+    cpp : "C++",
+    py : "Python",
+    java : "Java programming"
+}
+
+for (const key in obj8) {
+        // console.log(`${key} is a shortcut for ${obj8[key]}`);
+        }
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const every in programming) {
+    // console.log(programming[every]);
+    
+}
+
+const myMap = new Map();
+
+myMap.set("IN", "India");
+myMap.set("USA", "United States of America");
+myMap.set("FR", "France");
+myMap.set("FR", "France");
+
+for (const key in myMap) {
+    // console.log(key);
+    
+} // Not possible. Not iterable like this
+
+
+
+
+
+ 
+
