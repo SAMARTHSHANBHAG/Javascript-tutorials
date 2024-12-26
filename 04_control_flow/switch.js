f5e87a5b162e38468ad2c1d972f4991ab43831b7
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-26 16:16:41 +0530

    -

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,42 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 1:
+        console.log("January");
+        break;
+
+    default:
+        console.log("No match found!");
+        
+        break;
+}
