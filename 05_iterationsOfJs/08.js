3fd5e5a4da82c45579584cbe9bc0198d45cf319a
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-27 13:35:35 +0530

    -

---
+++ 05_iterationsOfJs/08.js
@@ -0,0 +1,36 @@
+const myNumbers = [1, 2, 3]
+
+const myTotal = myNumbers.reduce(function(acc, currval) {
+    // console.log(`acc : ${acc} and currval : ${currval}`);
+    
+    // return acc + currval
+}, 0)
+// console.log(myTotal);
+
+const myTotal1 = myNumbers.reduce( (acc, currval) => acc + currval, 0)
+// console.log(myTotal1);
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "py course",
+    price: 999,
+  },
+  {
+    itemName: "mobile dev course",
+    price: 5999,
+  },
+  {
+    itemName: "data science course",
+    price: 12999,
+  },
+]
+
+const PriceTopay = shoppingCart.reduce( (acc, item) => acc + item.price, 0 )
+console.log(PriceTopay);
+
+
+
