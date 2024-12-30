7be83a8830fc6640710af9a82e7c4a92c23b4a79
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-31 00:48:33 +0530

    -

--- 05_iterationsOfJs/02.js
+++ 05_iterationsOfJs/02.js
@@ -1,19 +1,19 @@
 // while loop
 
-// let i = 0
-// while (i <= 10) {
-//     console.log(`The value is ${i}`);
-//     i = i + 2
+let i = 0
+while (i <= 10) {
+    console.log(`The value is ${i}`);
+    i = i + 2
     
-// }
+}
 
-// let myArr = ['hit', 'fit', 'sit']
-// let arr = 0
+let myArr = ['hit', 'fit', 'sit']
+let arr = 0
 
-// while (arr < myArr.length) {
-//     console.log(`The value is ${myArr[arr]}`);
-//     arr = arr + 1
-// }
+while (arr < myArr.length) {
+    console.log(`The value is ${myArr[arr]}`);
+    arr = arr + 1
+}
 
 // Do while
 
