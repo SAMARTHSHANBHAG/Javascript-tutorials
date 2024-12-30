b0e22c8f8d2a9dff1a9e427c64c292ccca43e64a
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-30 10:53:42 +0530

    -

--- 01_BasicsOfJs/03_convertionOperation.js
+++ 01_BasicsOfJs/03_convertionOperation.js
@@ -1,57 +1,57 @@
-// let score = "33"
+let score = "33"
 
 // console.log(typeof score);
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 
-// let a = "22abc"
-// let numOfA = Number(a)
+let a = "22abc"
+let numOfA = Number(a)
 // console.log(numOfA); //Output: NaN
 
-// let b = null
-// let numOfB = Number(b)
+let b = null
+let numOfB = Number(b)
 // console.log(numOfB); //Output: 0
 
-// let bool = true
-// let numOfBool = Number(bool)
+let bool = true
+let numOfBool = Number(bool)
 // console.log(numOfBool); //Output: 1 for true, 0 for false
 
-// let name = "Samarth"
-// let numOfName = Number(name)
+let name = "Samarth"
+let numOfName = Number(name)
 // console.log(numOfName); // NaN
 
 // Note : "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
-// let isLoggedIn = false
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
+let isLoggedIn = false
+let booleanIsLoggedIn = Number(isLoggedIn)
 // console.log(booleanIsLoggedIn); // output 0
 
-// let emptyString = ""
-// let boolOfEmptyString = Boolean(emptyString)
+let emptyString = ""
+let boolOfEmptyString = Boolean(emptyString)
 // console.log(boolOfEmptyString); //Output: False
 
-// let isLoggedOff = "Java"
-// let booleanIsLoggedOff = Boolean(isLoggedOff)
+let isLoggedOff = "Java"
+let booleanIsLoggedOff = Boolean(isLoggedOff)
 // console.log(booleanIsLoggedOff); //Output : True
 
 // Note: 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-// let someNumber = 33
-// let stringNumber = String(someNumber)
+let someNumber = 33
+let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
 
 // *********************** Operations ***********************
 
-// let val = 3
-// let negVal = -val // Basic negation
-// console.log(negVal);
+let val = 3
+let negVal = -val // Basic negation
+// console.log(negVal); // -3
 
 // Basic arithmatic operations
 // console.log(2+2);
@@ -80,8 +80,8 @@ let str3 = str1 + str2
 // let num1, num2, num3;
 // num1 = num2 = num3 = 2 + 2; Bad code practices, we need good code readablity
 
-// let gameCounter = 100
-// gameCounter++; //Increment
+let gameCounter = 100
+gameCounter++; //Increment
 // console.log(gameCounter);
 
 
