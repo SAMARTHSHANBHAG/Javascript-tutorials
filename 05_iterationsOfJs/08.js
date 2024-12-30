2fbee64f37af54cdd5e48cb42a718b6aaef5c724
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2024-12-31 01:06:16 +0530

    -

--- 05_iterationsOfJs/08.js
+++ 05_iterationsOfJs/08.js
@@ -1,10 +1,10 @@
 const myNumbers = [1, 2, 3]
 
-const myTotal = myNumbers.reduce(function(acc, currval) {
-    // console.log(`acc : ${acc} and currval : ${currval}`);
+// const myTotal = myNumbers.reduce(function(acc, currval) {
+//     console.log(`acc : ${acc} and currval : ${currval}`);
     
-    // return acc + currval
-}, 0)
+//     return acc + currval
+// }, 0)
 // console.log(myTotal);
 
 const myTotal1 = myNumbers.reduce( (acc, currval) => acc + currval, 0)
