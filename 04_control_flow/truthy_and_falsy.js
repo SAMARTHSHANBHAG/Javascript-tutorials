2bc2b7531084163ed0f47e461d131e552eb1bf52
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-01-03 01:31:47 +0530

    -

--- 04_control_flow/truthy_and_falsy.js
+++ 04_control_flow/truthy_and_falsy.js
@@ -10,7 +10,7 @@ if (useremail) {
 
 // falsy values
 
-// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// false, 0, -0, BigInt 0n, " ", null, undefined, NaN
 
 //truthy values
 // "0", 'false', " ", [], {}, function(){}
