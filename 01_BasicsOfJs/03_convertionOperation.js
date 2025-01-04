34740d5193b13a059a4da057cb63abf39e8cdcfc
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-01-05 00:59:34 +0530

    -

--- 01_BasicsOfJs/03_convertionOperation.js
+++ 01_BasicsOfJs/03_convertionOperation.js
@@ -43,8 +43,8 @@ let booleanIsLoggedOff = Boolean(isLoggedOff)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(stringNumber); //33
+// console.log(typeof stringNumber); //string
 
 
 // *********************** Operations ***********************
