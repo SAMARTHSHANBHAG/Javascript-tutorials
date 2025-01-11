e728d919495c65d68beb7593530f8235206472ce
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-01-11 23:58:54 +0530

    -

--- 08_Advanced_Js/promises.js
+++ 08_Advanced_Js/promises.js
@@ -1,12 +1,12 @@
-const promiseOne = new Promise(function(resolve, reject){
-    // Do an async task
-    // Db calls, cryptography, network calls
-    setTimeout(function(){
-        console.log('Async task is complete');
-        resolve()
+// const promiseOne = new Promise(function(resolve, reject){
+//     // Do an async task
+//     // Db calls, cryptography, network calls
+//     setTimeout(function(){
+//         console.log('Async task is complete');
+//         resolve()
         
-    }, 1000)
-})
+//     }, 1000)
+// })
 
 promiseOne.then(function() {
     console.log("Promise has been consumed");
