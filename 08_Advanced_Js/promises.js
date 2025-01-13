03da67f444d40f559e7c700e72cda323964759bf
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-01-13 20:59:12 +0530

    -

--- 08_Advanced_Js/promises.js
+++ 08_Advanced_Js/promises.js
@@ -77,16 +77,28 @@ async function consumePromiseFive() {
 }
 consumePromiseFive()
 
-async function getAllUsers() {
-    try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = await response.json()
-        console.log(data);
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data = await response.json()
+//         console.log(data);
         
 
-    } catch (error) {
-        console.log("E: ", error);
+//     } catch (error) {
+//         console.log("E: ", error);
         
-    }
-}
-getAllUsers()
+//     }
+// }
+// getAllUsers()
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+    
+})
+.catch((error) => console.log(error)
+)
+
