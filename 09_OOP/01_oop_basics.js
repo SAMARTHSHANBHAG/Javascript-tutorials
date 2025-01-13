94d863eb5090c2b63c0cdaa5d4d831b16f0ba9cb
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-01-14 01:24:59 +0530

    -

--- 09_OOP/01_oop_basics.js
+++ 09_OOP/01_oop_basics.js
@@ -6,10 +6,13 @@ const user = {
 
     getUserdetails : function() {
         // console.log("Got user details");
-        console.log(`Username : ${this.username}`);
+        // console.log(`Username : ${this.username}`);
+        console.log(this); // returns you the entire object
+        
         
         
     }
 }
 console.log(user.username);
-console.log(user.getUserdetails());
+// console.log(user.getUserdetails());
+// console.log(this) // empty object, has no context
