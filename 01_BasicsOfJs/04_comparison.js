c021a5b819251eaae6b85be220341fa9d053b243
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-03-29 11:22:39 +0530

    -

--- 01_BasicsOfJs/04_comparison.js
+++ 01_BasicsOfJs/04_comparison.js
@@ -1,15 +1,15 @@
-console.log(2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
 
-console.log("2" > 1); //true
-console.log("02" > 1); //true
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+console.log(null == 0); // null == 0 is false since null is only equal to undefined in loose equality (==).
+console.log(null >= 0); // null >= 0 is true because null is converted to 0 in comparison operations (>=), making 0 >= 0 evaluate to true.
 
 console.log(undefined == 0);
 console.log(undefined > 0);
