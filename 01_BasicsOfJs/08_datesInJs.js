a132aaa9a9076e4b5134be6e397f4513f52f65ed
SAMARTHSHANBHAG <samarthr.shanbhag@gmail.com>
2025-04-02 14:52:14 +0530

    -

--- 01_BasicsOfJs/08_datesInJs.js
+++ 01_BasicsOfJs/08_datesInJs.js
@@ -16,14 +16,14 @@ let myCreatedDate3 = new Date("01-14-2023")
 // console.log(myCreatedDate3.toLocaleString());
 
 let myTimeStamp = Date.now()
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/10000000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getDay());
-console.log(newDate.getMonth() + 1);
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
 
 // newDate.toLocaleString('default', {
 //     weekday: "long"
